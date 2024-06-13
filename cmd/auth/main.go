17aec335cd6f27d6c99d2713c434fe2ae43b876c
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-06-13 14:55:01 -0300

    chore: remove migration from main

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/pgx"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/lucas_cda/go-acai-microservices/internal/auth"
+
 	"github.com/lucas_cda/go-acai-microservices/pkg/postgres"
 )
 
@@ -17,14 +15,6 @@ func main() {
 		panic(err)
 	}
 	defer pg.Close()
-	log.Print("Migrating database...")
-	m, err := migrate.New("file://db/migrations", "pgx://root:example@localhost:5432/db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := m.Up(); err != nil {
-		log.Fatal(err)
-	}
 
 	r := gin.Default()
 	api := r.Group("/api/v1/auth")
